51d4ad4983419d6e8b29be88498477554b4f008b
R.Frega <robertofrega@gmail.com>
2021-04-28 18:01:49 -0300

    style: give it a slight facelift

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css';
 import useFetch from 'use-http'
-import {Avatar, Container, Grid, List, ListItem, TextField, Typography} from "@material-ui/core";
+import {Avatar, Box, Grid, List, ListItem, Paper, TextField, Typography} from "@material-ui/core";
 import * as React from "react";
 import {useEffect, useMemo, useState} from "react";
 
 
 export default function App() {
-    const [username, setUsername] = useState('fregadev')
+    const [username, setUsername] = useState('')
     const {response: profile, error: profileError} = useFetch(`https://api.github.com/users/${username}`, [username])
     const {
         response: stars,
@@ -39,31 +39,34 @@ export default function App() {
         setUsername(e.target.value)
     };
     return (
-        <Container>
-            <Grid container spacing={2} direction="column">
-                <Grid item>
-                    <Typography>github.com/</Typography>
-                    <TextField value={username} onChange={onChangeTextField}/>
-                </Grid>
-                <Grid item>
-                    {
-                        !starsError && !profileError && profile.data && stars.data && <Grid container>
-                            <Grid item>
-                                <Avatar src={profile.data.avatar_url}/>
-                            </Grid>
-                            <Grid item>
-                                <List><
-                                    ListItem>Followers: {profile.data.followers}</ListItem>
-                                    <ListItem>Public Repositories: {profile.data.public_repos}</ListItem>
-                                    <ListItem>Stars: {stars.data.length}</ListItem>
-                                    <ListItem>Watchers: {watchers}</ListItem>
-                                    <ListItem>Forks: {forks}</ListItem>
-                                </List>
-                            </Grid>
+        <Box display={'flex'} height={'100vh'} alignItems={'center'} justifyContent={'center'}>
+            <Paper elevation={6}>
+                <Box p={10}>
+                    <Grid container spacing={2} direction="column">
+                        <Grid item>
+                            <Typography>github.com/</Typography>
+                            <TextField value={username} onChange={onChangeTextField} label={'username'}/>
                         </Grid>
-                    }
-                </Grid>
-            </Grid>
-        </Container>
+                        <Grid item>
+                            {
+                                !starsError && !profileError && profile.data && stars.data && <Grid container>
+                                    <Grid item>
+                                        <Avatar src={profile.data.avatar_url}/>
+                                    </Grid>
+                                    <Grid item>
+                                        <List><
+                                            ListItem>Followers: {profile.data.followers}</ListItem>
+                                            <ListItem>Public Repositories: {profile.data.public_repos}</ListItem>
+                                            <ListItem>Stars: {stars.data.length}</ListItem>
+                                            <ListItem>Watchers: {watchers}</ListItem>
+                                            <ListItem>Forks: {forks}</ListItem>
+                                        </List>
+                                    </Grid>
+                                </Grid>
+                            }
+                        </Grid>
+                    </Grid></Box>
+            </Paper>
+        </Box>
     );
 }
